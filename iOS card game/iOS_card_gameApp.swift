f19853db8d3d93499e79d18adf2de903f2f7c564
rohitchoudhary1243 <rohitdudi7023@gmail.com>
2024-07-29 10:25:28 +0530

    Initial Commit

---
+++ iOS card game/iOS_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_card_gameApp.swift
+//  iOS card game
+//
+//  Created by RohitDudi on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
